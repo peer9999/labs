d4773289166ea3faf4cca6d4cc07be5c79eb11fc
unknown <gribovskij.01@gmail.com>
2020-07-11 17:10:30 +0300

    Kursach

---
+++ 1 kurs/2 sem/OAiP/kursovaya/Project4/menu.h
@@ -0,0 +1,14 @@
+enum MENU {
+    INPUT = 1,
+    SORT,
+    DELETE,
+    WRITE,
+    READ,
+    EDIT,
+    FIND,
+    EXIT = 0
+};
+
+void MenuView  (); //функция для отображения меню
+int  MenuSelect(); //функция выбора действия
+
